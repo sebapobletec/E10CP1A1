eb41a782f42147090f9bcf5a0caf433d6cf7acc3
Sebastian Poblete <sebapobletec@gmail.com>
2019-01-07 23:47:09 -0300

    Ejercicio6

---
+++ Ejercicio6.rb
@@ -0,0 +1,36 @@
+module Test
+  def result
+    promedio = (@nota1 + @nota2) / 2
+    if promedio >= 4
+      puts 'Estudiante aprobado'
+    else puts 'Estuadiante reprobado'
+    end
+  end
+end
+
+module Attendance
+  def student_quantity
+    class_variable_get(:@@quantity)
+  end
+
+end
+
+class Student
+  @@quantity = 0
+  include Test
+  extend Attendance
+  def initialize(nombre, nota1 = 4, nota2 = 4)
+    @nombre = nombre
+    @nota1 = nota1.to_i
+    @nota2 = nota2.to_i
+    @@quantity += 1
+  end
+end
+
+students = []
+10.times do
+students << Student.new('Juan', 4, 5)
+end
+
+puts students[0].result
+puts Student.student_quantity
